a3f72b520e6345afc0da5f83ddcc3dd572a744c5
morevivo <morrevivo2@gmail.com>
2022-03-04 00:36:29 +0200

    log in, log out, welcome and navigation

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomePage from './components/welcomePage/WelcomePage';
+import SignIn from './components/SignIn/SignIn'
+import SignUp from './components/SignUp/SignUp'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'white'
+  },
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Welcome"
+          component={WelcomePage}
+        />
+        <Stack.Screen
+          name="Sign In"
+          component={SignIn}
+        />
+        <Stack.Screen
+          name="Sign Up"
+          component={SignUp}
+        />
+      </Stack.Navigator>
+      {/* <View style={styles.container}>
+        <SignIn></SignIn>
+        <WelcomePage navigation={Stack}></WelcomePage>
+        <SignUp></SignUp>
+        <StatusBar style="auto" />
+      </View> */}
+    </NavigationContainer>
   );
 }
 
---
+++ components/welcomePage/WelcomePage.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Pressable, ActivityIndicator } from 'react-native';
+import { Button, Text, Image, useTheme } from 'react-native-elements';
+import SignIn from '../SignIn/SignIn'
+import SignUp from '../SignUp/SignUp'
+
+
+
+
+export default function WelcomePage({navigation} ) {
+    const { theme } = useTheme();
+    const [isSignInOpen, setIsSignInOpen] = useState(false)
+    const [isSignUpOpen, setIsSignUpOpen] = useState(false)
+
+
+    return (
+        <View style={styles.container}>
+            <Text
+                style={styles.text}
+                h1
+                h1Style={{ color: theme?.colors?.primary }}
+            >
+                TODO-Bom
+            </Text>
+            <View style={styles.imageView}> 
+                <Image style={styles.image}
+                    source={require("./todoP.jpeg")}
+                    containerStyle={styles.imageContainer}
+                    PlaceholderContent={<ActivityIndicator />}
+                />
+            </View>
+            
+            <Button
+                title={'Sign In'}
+                containerStyle={{
+                    width: 200,
+                    marginHorizontal: 50,
+                    marginVertical: 10,
+                }}
+                onPress={() => navigation.navigate('Sign In')}
+            />
+            <Button
+                title={'Sign Up'}
+                containerStyle={{
+                    width: 200,
+                    marginHorizontal: 50,
+                    marginVertical: 10,
+                }}
+                onPress={() => navigation.navigate('Sign Up')}
+            />
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    image: {
+        flex:1,
+        resizeMode: 'contain'
+    },
+    text: {
+        textAlign: 'center',
+        padding: 2,
+    },
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        // justifyContent: 'space-between',
+        height: '100%',
+        width: '100%',
+
+    },
+    imageContainer:{
+        height: '100%',
+        width:'100%'
+    },
+    imageView:{
+        height: '50%',
+        width:'80%'
+    }
+});
+
+
+
