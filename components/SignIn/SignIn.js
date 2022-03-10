75001a85056b473915a35b05000c10489bbf3d2a
morevivo <morrevivo2@gmail.com>
2022-03-10 14:23:28 +0200

    sign in and sign up with validation and navigation - server conaction problems

--- components/SignIn/SignIn.js
+++ components/SignIn/SignIn.js
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Pressable, ActivityIndicator, TextInput } from 'react-native';
 import { Button, Text, Image, useTheme, Input } from 'react-native-elements';
+import { Login } from '../Services/AuthServices'
 
 export default function SignIn() {
     const { theme } = useTheme();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isDisable, setIsDisable] = useState(true);
+
+    useEffect(() => {
+        console.log(ValidateEmail())
+        console.log(email.length)
+        console.log(password.length)
+        ValidateEmail() && email.length && password.length ? setIsDisable(false) : setIsDisable(true)
+    }, [email,password])
 
     const ValidateEmail = () => {
         if (!email.length) return true
         const validCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/.test(email)
         return validCheck
     }
-
+    const HandleSubmit = async () => {
+        const loginRequest = {
+            "email": email,
+            "password": password,
+        }
+        let a = await Login(loginRequest)
+        console.log("@")
+        console.log("1" + a)
+        console.log("2" + a.status)
+        console.log("3" + a.message)
+    }
 
     return (
-        <View style={{ alignItems: 'center', marginTop:'10%' }}>
+        <View style={{ alignItems: 'center', marginTop: '10%' }}>
             <Text
                 h1
                 h1Style={{ color: theme?.colors?.primary }}
@@ -23,7 +42,7 @@ export default function SignIn() {
                 Your Account
             </Text>
             <Input
-                containerStyle={{ width: 250,marginTop:'10%' }}
+                containerStyle={{ width: 250, marginTop: '10%' }}
                 placeholder='email@address.com'
                 leftIcon={{ type: 'font-awesome', name: 'envelope' }}
                 errorMessage={ValidateEmail() ? "" : "Email is invalid"}
@@ -34,6 +53,7 @@ export default function SignIn() {
                 placeholder='Password'
                 leftIcon={{ type: 'font-awesome', name: 'lock' }}
                 onChangeText={value => setPassword(value)}
+                secureTextEntry={true}
             />
             <Button
                 title={'Sign In'}
@@ -42,6 +62,8 @@ export default function SignIn() {
                     marginHorizontal: 50,
                     marginVertical: 20,
                 }}
+                onPress={HandleSubmit}
+                disabled={isDisable}
             />
         </View>
     )
--- components/SignUp/SignUp.js
+++ components/SignUp/SignUp.js
@@ -2,10 +2,13 @@ import { LineWeight } from '@material-ui/icons';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Pressable, ActivityIndicator } from 'react-native';
 import { Button, Text, Image, useTheme, Input, Avatar } from 'react-native-elements';
+import { RegisterUser } from '../Services/AuthServices';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export default function SignUp() {
     const { theme } = useTheme();
     const LineWidth = 290
+    const [image, setImage] = useState("");
     const [name, setName] = useState("");
     const [date, setDate] = useState("");
     const [email, setEmail] = useState("");
@@ -13,97 +16,128 @@ export default function SignUp() {
     const [confPassword, setConfPassword] = useState("");
     const [IsDisable, setIsDisable] = useState("");
 
+    useEffect(() => {
+        ValidateDate() && ValidateEmail() && password.length > 6 && password.length && password == confPassword ?
+            setIsDisable(false) : setIsDisable(true)
+    }, [name, date, email, password, confPassword])
+
+    const HandleSubmit = async () => {
+        const regisretRequest = {
+            "email": email,
+            "password": password,
+            "display_name": name,
+            "birth_date": date,
+            "image": "https://some-image.com"
+        }
+        let a = await RegisterUser(regisretRequest)
+        console.log("@")
+        console.log("1" + a)
+        console.log("2" + a.status)
+        console.log("3" + a.message)
+    }
+
     const ValidateEmail = () => {
         if (!email.length) return true
         const validCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/.test(email)
-        setIsDisable(!validCheck? true:false) 
         return validCheck
     }
 
     const ValidateDate = () => {
-            if (!date.length) return true
-            // First check for the pattern
-            if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(date))
-                return false;
-            // Parse the date parts to integers
-            var parts = date.split("/");
-            var day = parseInt(parts[1], 10);
-            var month = parseInt(parts[0], 10);
-            var year = parseInt(parts[2], 10);
-            // Check the ranges of month and year
-            if (year < 1000 || year > 3000 || month == 0 || month > 12)
-                return false;
-            var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-            // Adjust for leap years
-            if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-                monthLength[1] = 29;
-            // Check the range of the day
-            return day > 0 && day <= monthLength[month - 1];
-        }
+        if (!date.length) return true
+        // First check for the pattern
+        if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(date))
+            return false;
+        // Parse the date parts to integers
+        var parts = date.split("/");
+        var day = parseInt(parts[1], 10);
+        var month = parseInt(parts[0], 10);
+        var year = parseInt(parts[2], 10);
+        // Check the ranges of month and year
+        if (year < 1000 || year > 3000 || month == 0 || month > 12)
+            return false;
+        var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        // Adjust for leap years
+        if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+            monthLength[1] = 29;
+        // Check the range of the day
+        return day > 0 && day <= monthLength[month - 1];
+    }
 
 
     return (
-        <View style={{alignItems:"center",display:"flex", marginTop:'10%'}}>
-            <Text
-                // style={styles.text}
-                h1
-                h1Style={{ color: theme?.colors?.primary }}
-            >
-                Create Account
-            </Text>
-            <View style={{ display: "flex", alignItems: "center", margin: '5%' }}>
-                <Avatar
-                    size={64}
-                    rounded
-                    icon={{ name: 'adb', type: 'material' }}
-                    containerStyle={{ backgroundColor: 'orange' }}
+        <KeyboardAwareScrollView keyboardShouldPersistTaps={'always'}
+            style={{ flex: 1 }}
+            showsVerticalScrollIndicator={false}>
+            <View style={{ alignItems: "center", display: "flex", marginTop: '10%' }}>
+                <Text
+                    h1
+                    h1Style={{ color: theme?.colors?.primary }}
                 >
-                    <Avatar.Accessory size={24} />
-                </Avatar>
+                    Create Account
+                </Text>
+                <View style={{ display: "flex", alignItems: "center", margin: '5%' }}>
+                    <Avatar
+                        size={64}
+                        rounded
+                        icon={{ name: 'adb', type: 'material' }}
+                        containerStyle={{ backgroundColor: 'orange' }}
+                    >
+                        <Avatar.Accessory size={24} />
+                    </Avatar>
+                </View>
+                <Input
+                    containerStyle={{ width: LineWidth }}
+                    placeholder='Name'
+                    leftIcon={{ type: 'font-awesome', name: 'user' }}
+                    onChangeText={value => setName(value)}
+                />
+                <Input
+                    containerStyle={{ width: LineWidth }}
+                    placeholder='mm/dd/yyy'
+                    leftIcon={{ type: 'font-awesome', name: 'calendar' }}
+                    errorMessage={ValidateDate() ? "" : "date is invalid"}
+                    onChangeText={value => setDate(value)}
+                />
+                <Input
+                    containerStyle={{ width: LineWidth }}
+                    placeholder='Email@address.com'
+                    leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+                    errorMessage={ValidateEmail() ? "" : "Email is invalid"}
+                    onChangeText={value => setEmail(value)}
+                />
+                <Input
+                    containerStyle={{ width: LineWidth }}
+                    placeholder='Password(min 7 chars)'
+                    leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                    errorMessage={password.length > 6 || !password.length ? "" : "Password is under 7 chars"}
+                    onChangeText={value => setPassword(value)}
+                    secureTextEntry={true}
+                />
+                <Input
+                    containerStyle={{ width: LineWidth }}
+                    placeholder='Confirm Password'
+                    leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                    errorMessage={password == confPassword || !confPassword.length ? "" : "Passwords are not equals"}
+                    onChangeText={value => setConfPassword(value)}
+                    secureTextEntry={true}
+                />
+                <Button
+                    title={'Sign Up'}
+                    containerStyle={{
+                        width: 200,
+                        marginHorizontal: 50,
+                        marginVertical: 10,
+                    }}
+                    disabled={IsDisable}
+                    onPress={HandleSubmit}
+                />
             </View>
-            <Input
-                containerStyle={{ width: LineWidth }}
-                placeholder='Name'
-                leftIcon={{ type: 'font-awesome', name: 'user' }}
-                onChangeText={value => setName(value)}
-            />
-            <Input
-                containerStyle={{ width: LineWidth }}
-                placeholder='mm/dd/yyy'
-                leftIcon={{ type: 'font-awesome', name: 'calendar' }}
-                errorMessage={ValidateDate() ? "" : "date is invalid"}
-                onChangeText={value => setDate(value)}
-            />
-            <Input
-                containerStyle={{ width: LineWidth }}
-                placeholder='Email@address.com'
-                leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-                errorMessage={ValidateEmail() ? "" : "Email is invalid"}
-                onChangeText={value => setEmail(value)}
-            />
-            <Input
-                containerStyle={{ width: LineWidth }}
-                placeholder='Password(min 7 chars)'
-                leftIcon={{ type: 'font-awesome', name: 'lock' }}
-                errorMessage={password.length > 6 || !password.length? "" : "Password is under 7 chars"}
-                onChangeText={value => setPassword(value)}
-            />
-            <Input
-                containerStyle={{ width: LineWidth }}
-                placeholder='Confirm Password'
-                leftIcon={{ type: 'font-awesome', name: 'lock' }}
-                errorMessage={password == confPassword || !confPassword.length? "" : "Passwords are not equals"}
-                onChangeText={value => setConfPassword(value)}
-            />
-            <Button
-                title={'Sign Up'}
-                containerStyle={{
-                    width: 200,
-                    marginHorizontal: 50,
-                    marginVertical: 10,
-                }}
-            />
-        </View>
+        </KeyboardAwareScrollView>
+
+
+
+
+
     )
 }
 
