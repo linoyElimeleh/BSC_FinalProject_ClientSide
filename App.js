d607f6dd485fbbd6ba7ff0d764ead0c40e164726
Yosi Arbiv <yosia@kovrr.com>
2022-04-23 15:37:53 +0300

    added some notifications

--- App.js
+++ App.js
@@ -1,79 +1,163 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
-import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import WelcomePage from './components/welcomePage/WelcomePage';
-import { HeaderBackButton } from '@react-navigation/elements';
-import SignIn from './components/SignIn/SignIn';
-import SignUp from './components/SignUp/SignUp';
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import React, { useState, useEffect, useRef } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import WelcomePage from "./components/welcomePage/WelcomePage";
+import { HeaderBackButton } from "@react-navigation/elements";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
 
-import {GroupsScreen, AddGroup, GroupCreated, JoinGroup} from './components/Groups'
+import {
+    GroupsScreen,
+    AddGroup,
+    GroupCreated,
+    JoinGroup,
+} from "./components/Groups";
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});
 
 const Stack = createNativeStackNavigator();
 const MyTheme = {
     ...DefaultTheme,
     colors: {
         ...DefaultTheme.colors,
-        background: 'white'
+        background: "white",
     },
-}
+};
 
 export default function App() {
+    const [expoPushToken, setExpoPushToken] = useState("");
+    const [notification, setNotification] = useState(false);
+    const notificationListener = useRef();
+    const responseListener = useRef();
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then((token) =>
+            setExpoPushToken(token)
+        );
+
+        // This listener is fired whenever a notification is received while the app is foregrounded
+        notificationListener.current =
+            Notifications.addNotificationReceivedListener((notification) => {
+                setNotification(notification);
+            });
+
+        // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+        responseListener.current =
+            Notifications.addNotificationResponseReceivedListener(
+                (response) => {
+                    console.log(response);
+                }
+            );
+
+        return () => {
+            Notifications.removeNotificationSubscription(
+                notificationListener.current
+            );
+            Notifications.removeNotificationSubscription(
+                responseListener.current
+            );
+        };
+    }, []);
+
     return (
         <NavigationContainer theme={MyTheme}>
             <Stack.Navigator>
-                <Stack.Screen
-                    name="Welcome Page"
-                    component={WelcomePage}
-                />
-                <Stack.Screen
-                    name="Sign In"
-                    component={SignIn}
-                />
-                <Stack.Screen
-                    name="Sign Up"
-                    component={SignUp}
-                />
+                <Stack.Screen name="Welcome Page" component={WelcomePage} />
+                <Stack.Screen name="Sign In" component={SignIn} />
+                <Stack.Screen name="Sign Up" component={SignUp} />
                 <Stack.Screen
                     name="Groups"
                     component={GroupsScreen}
-                    options={({navigation}) =>
-                        (
-                            {
-                                headerLeft: () => {}
-                            }
-                        )
-                    }
+                    options={({ navigation }) => ({
+                        headerLeft: () => {},
+                    })}
                 />
-                <Stack.Screen
-                    name="Create Group"
-                    component={AddGroup}
-                />
-                <Stack.Screen
-                    name="Join Group"
-                    component={JoinGroup}
-                />
+                <Stack.Screen name="Create Group" component={AddGroup} />
+                <Stack.Screen name="Join Group" component={JoinGroup} />
                 <Stack.Screen
                     name="Group Created"
                     component={GroupCreated}
-                    options={({navigation}) =>
-                        (
-                            {
-                                headerLeft: () => (<HeaderBackButton onPress={()=> navigation.navigate('Groups')}/>)
-                            }
-                        )
-                    }
+                    options={({ navigation }) => ({
+                        headerLeft: () => (
+                            <HeaderBackButton
+                                onPress={() => navigation.navigate("Groups")}
+                            />
+                        ),
+                    })}
                 />
             </Stack.Navigator>
         </NavigationContainer>
     );
 }
 
+// Can use this function below, OR use Expo's Push Notification Tool-> https://expo.dev/notifications
+async function sendPushNotification(expoPushToken) {
+    const message = {
+        to: expoPushToken,
+        sound: "default",
+        title: "Original Title",
+        body: "And here is the body!",
+        data: { someData: "goes here" },
+    };
+
+    await fetch("https://exp.host/--/api/v2/push/send", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Accept-encoding": "gzip, deflate",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(message),
+    });
+}
+
+async function registerForPushNotificationsAsync() {
+    let token;
+    if (Device.isDevice) {
+        const { status: existingStatus } =
+            await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== "granted") {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== "granted") {
+            alert("Failed to get push token for push notification!");
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+    } else {
+        alert("Must use physical device for Push Notifications");
+    }
+
+    if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+            name: "default",
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: "#FF231F7C",
+        });
+    }
+
+    return token;
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
