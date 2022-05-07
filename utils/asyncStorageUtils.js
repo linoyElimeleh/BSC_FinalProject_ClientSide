c302da412a16ae0abf69c97d9cdfb23ed65ab66e
Shir Agami <shirag2007@gmail.com>
2022-05-07 20:02:59 +0300

    ✨ Introducing: push notifications

--- utils/asyncStorageUtils.js
+++ utils/asyncStorageUtils.js
@@ -2,31 +2,29 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const getData = async (key) => {
     try {
-        const value = await AsyncStorage.getItem(key)
+        const value = await AsyncStorage.getItem(key);
         if (value !== null) {
-            return (value)
+            return value;
         }
     } catch (e) {
-        console.log('AsyncStorage get error: ' + error.message);
+        console.log("AsyncStorage get error: " + error.message);
     }
 };
 
 export const storeData = async (key, value) => {
     try {
-        await AsyncStorage.setItem(key, value)
+        await AsyncStorage.setItem(key, value);
     } catch (e) {
-        console.log('AsyncStorage set error: ' + error.message);
+        console.log("AsyncStorage set error: " + error.message);
     }
-}
+};
 
 export const removeData = async (key) => {
-    try{
+    try {
         await AsyncStorage.removeItem(key);
         return true;
-    }
-    catch (e){
-        console.log('AsyncStorage remove error: ' + error.message);
+    } catch (e) {
+        console.log("AsyncStorage remove error: " + error.message);
         return false;
     }
-}
-
+};
